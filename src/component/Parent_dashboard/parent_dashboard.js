2279c24a35b7d20fe16b1d13fcbb6f675a9b66cb
AlexanderOba <alexzy4ak@gmail.com>
2021-04-24 17:32:19 -0700

    added functionality to customized accordion

--- src/component/Parent_dashboard/parent_dashboard.js
+++ src/component/Parent_dashboard/parent_dashboard.js
@@ -1,9 +1,8 @@
-import React from "react";
-import { Container, Row, Col,Card } from "react-bootstrap";
-import Accordion from "react-bootstrap/Accordion";
+import React, { useContext } from "react";
+import { Container, Row, Col, Card } from "react-bootstrap";
 import "./parent_dashboard.css";
 import { Link } from "react-router-dom";
-import arrowdown from "../../images/dwn-arrw.png";
+import Accordion from '../widget/accordion';
 
 const parentDashboard = () => {
   return (
@@ -12,36 +11,7 @@ const parentDashboard = () => {
         <Row>
           <Col md={3} className="sidenav">
             <div className="sidenavpaddin">
-              <Accordion defaultActiveKey="" className="userprofileacc">
-                <Accordion.Toggle eventKey="5" as={Card.Header}>
-                <div  className="userInfo-section" >
-                  <div class="initials-tag">
-                    <span>DA</span>
-                  </div>
-                  <div className="logosctncontent">
-                    <div className="acctgglediv">
-                      <h4>Toptutors</h4>
-                      <div>
-                        <img src={arrowdown} />
-                      </div>
-                    </div>
-                    <p>David Armstrong - Admin</p>
-                  </div>
-                  </div>
-                </Accordion.Toggle>
-                <Accordion.Collapse className="acccollapsediv" eventKey="5">
-                <div>
-                  <div className="editprofdiv">
-                    <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
-                    <span className="edittag">Edit Profile</span>
-                  </div>
-                  <div className="editprofdiv">
-                    <i class="fa fa-sign-out" aria-hidden="true"></i>
-                    <span className="edittag" style={{marginBottom: "13px"}}>Log Out</span>
-                  </div>
-                  </div>
-                </Accordion.Collapse>
-              </Accordion>
+              <Accordion/>
             </div>
           </Col>
         </Row>
---
+++ src/component/widget/accordion.js
@@ -0,0 +1,63 @@
+import React, { useState, useRef } from "react";
+import { Card } from "react-bootstrap";
+
+const Accordion = () => {
+  const [state, setState] = useState({
+    active: "",
+    collapseHeight: "0px",
+    chevron: ""
+  });
+  const { active, collapseHeight, chevron } = state;
+  const content = useRef();
+  const toggleAccordion = () => {
+    setState({
+      ...state,
+      active: active === "" ? "active" : "",
+      collapseHeight: active === "active" ? "0px" : `${content.current.scrollHeight}px`,
+      chevron: active === "active" ? "" : "arrowflip" 
+    });
+  };
+
+  return (
+    <div>
+      <div className="userprofileacc">
+        <div as={Card.Header}>
+          <div className="userInfo-section">
+            <div class="initials-tag">
+              <span>DA</span>
+            </div>
+            <div
+              className={`logosctncontent ${active}`}
+              onClick={toggleAccordion}
+            >
+              <div className="acctgglediv">
+                <h4>Toptutors</h4>
+                <div>
+                  <span className={`fa fa-chevron-down arrow-down ${chevron}`}></span>
+                </div>
+              </div>
+              <p className="username">David Armstrong - Admin</p>
+            </div>
+          </div>
+        </div>
+        <div
+          className="acccollapsediv"
+          style={{ maxHeight: `${collapseHeight}` }}
+          ref={content}
+        >
+          <div className="editprofdiv">
+            <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+            <span className="edittag">Edit Profile</span>
+          </div>
+          <div className="editprofdiv">
+            <i class="fa fa-sign-out" aria-hidden="true"></i>
+            <span className="edittag" style={{ marginBottom: "13px" }}>
+              Log Out
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Accordion;
