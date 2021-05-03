a93e9e0f7be90962b52f189afb99913862fdb968
AlexanderOba <alexzy4ak@gmail.com>
2021-05-03 10:08:13 -0700

    test dashbard in progress..

---
+++ src/component/Parent_dashboard/DashboardSidenav.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import "./parent_dashboard.css";
+import { NavLink } from "react-router-dom";
+
+const DashboardSidenav = () => {
+  return (
+    <div>
+      <NavLink
+        to="/parent_dashboard"
+        className="sidenavlinkdiv"
+        activeStyle={{
+          backgroundColor: "#0675c1",
+          color: "#fff",
+          textDecoration: "none",
+          display: "block",
+          borderRadius: "5px"
+        }}
+      >
+        <span className="fa fa-home sidenavicons"></span>
+        Dashboard
+      </NavLink>
+      <div className="sidenavlinkdiv">
+        <span className="fa fa-search sidenavicons"></span>
+        Find a tutor
+      </div>
+      <div className="sidenavlinkdiv">
+        <span className="fa fa-file-archive-o sidenavicons"></span>
+        Bookings
+      </div>
+      <div className="sidenavlinkdiv">
+        <span className="fa fa-users sidenavicons"></span>
+        Your students
+      </div>
+      <div className="sidenavlinkdiv">
+        <span className="fa fa-envelope-open-o sidenavicons"></span>
+        Messages
+      </div>
+      <div className="sidenavlinkdiv">
+        <span className="fa fa-gift sidenavicons"></span>
+        Refer a friend
+      </div>
+      <div className="sidenavdemarcation"></div>
+      <div className="sidenavlinkdiv">
+        <span className="fa fa-exclamation-circle fa-rotate-180 sidenavicons"></span>
+        Help center
+      </div>
+      <div className="sidenavlinkdiv">
+        <span className="fa fa-info-circle sidenavicons"></span>
+        Report concern
+      </div>
+      <div className="sidenavlinkdiv" style={{ marginBottom: "100px" }}>
+        <span className="fa fa-phone-square sidenavicons"></span>
+        contact us
+      </div>
+    </div>
+  );
+};
+export default DashboardSidenav;
--- src/component/Parent_dashboard/parent_dashboard.js
+++ src/component/Parent_dashboard/parent_dashboard.js
@@ -1,8 +1,10 @@
-import React, { useContext } from "react";
-import { Container, Row, Col, Card } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import "./parent_dashboard.css";
-import { Link } from "react-router-dom";
-import Accordion from '../widget/accordion';
+import Accordion from "../widget/accordion";
+import DashboardSidenav from "./DashboardSidenav";
+import drpdwnarr from "../../images/dwn-arrw.png";
+import yoga from "../../images/yoga.png";
 
 const parentDashboard = () => {
   return (
@@ -11,49 +13,147 @@ const parentDashboard = () => {
         <Row>
           <Col md={3} className="sidenav">
             <div className="sidenavpaddin">
-              <Accordion/>
-              <div className="sidenavlinkdiv">
-                <span className="fa fa-home sidenavicons"></span>
-                Dashboard
-              </div>
-              <div className="sidenavlinkdiv">
-                <span className="fa fa-search sidenavicons"></span>
-                Find a tutor
-              </div>
-              <div className="sidenavlinkdiv">
-                <span className="fa fa-file-archive-o sidenavicons"></span>
-                Bookings
-              </div>
-              <div className="sidenavlinkdiv">
-                <span className="fa fa-users sidenavicons"></span>
-                Your students
-              </div>
-              <div className="sidenavlinkdiv">
-                <span className="fa fa-envelope-open-o sidenavicons"></span>
-                Messages
-              </div>
-              <div className="sidenavlinkdiv">
-                <span className="fa fa-gift sidenavicons"></span>
-                Refer a friend
-              </div>
-              <div className="sidenavdemarcation"></div>
-              <div className="sidenavlinkdiv">
-                <span className="fa fa-exclamation-circle fa-rotate-180 sidenavicons"></span>
-                Help center
-              </div>
-              <div className="sidenavlinkdiv">
-                <span className="fa fa-info-circle sidenavicons"></span>
-                Report concern
-              </div>
-              <div className="sidenavlinkdiv">
-                <span className="fa fa-phone-square sidenavicons"></span>
-                contact us
-              </div>
+              <Accordion />
+              <DashboardSidenav />
             </div>
           </Col>
           <Col md={9} className="dashboard-content">
-          <div>
-          </div>
+            <div className="user-imgdisplaydv">
+              <div className="useravatardiv">
+                <span className="userimgspan">
+                  <i className="fa fa-user"> </i>
+                  {/* <img src={art} className="img-fluid"/> */}
+                </span>
+              </div>
+              <div className="useravatardiv">David Armstrong</div>
+              <div className="useravatardiv">
+                <i className="fa fa-bell" style={{ color: "#70757a" }}>
+                  {" "}
+                </i>
+              </div>
+            </div>
+            <h3>Hello, David</h3>
+            <Row>
+              <Col md={12}>
+                <div className="container-card">
+                  <div className="tutor-search-wrapper">
+                    <Row>
+                      <Col md={4} className="dshbdfindatutorcontent">
+                        <img src="https://cdn.mytutor.co.uk/icons/search.svg?1619727184000" />
+                        <div className="findftutordiv">
+                          <h3>Find a Tutor</h3>
+                          <p>
+                            Browse tutor profiles and message a few you’d like
+                            to meet.
+                          </p>
+                        </div>
+                      </Col>
+                      <Col md={8}>
+                        <Row>
+                          <Col md={4} className="slect-inputwrapper">
+                          <label className="tutorsearchlabel">Subjects</label>
+                            <select
+                              className="subjectselectinpt rdseltinpt form-control"
+                              required
+                            >
+                              <option
+                                value=""
+                                className="rdsltopt"
+                                disabled
+                                selected
+                                hidden
+                              >
+                                All Level
+                              </option>
+                              <option value=" Biology" className="rdsltopt">
+                                Biology
+                              </option>
+                              <option value="Physics" className="rdsltopt">
+                                Physics
+                              </option>
+                              <option value=" Chemistry" className="rdsltopt">
+                                Chemistry
+                              </option>
+                              <option
+                                value="Mathematics"
+                                className="rdsltopt"
+                              >
+                              Mathematics
+                              </option>
+                              <option value="4" className="rdsltopt">
+                                Friend
+                              </option>
+                            </select>
+                            <div className="text-right">
+                              <img src={drpdwnarr} className="drparr" />
+                            </div>
+                            
+                          </Col>
+                          <Col md={4} className="slect-inputwrapper">
+                          <label className="tutorsearchlabel">Level</label>
+                            <select
+                              className="subjectselectinpt rdseltinpt form-control"
+                              required
+                            >
+                              <option
+                                value=""
+                                className="rdsltopt"
+                                disabled
+                                selected
+                                hidden
+                              >
+                                All Subjects
+                              </option>
+                              <option value="Facebook" className="rdsltopt">
+                                Facebook
+                              </option>
+                              <option value="Linkedin" className="rdsltopt">
+                                Linkedin
+                              </option>
+                              <option value="Instagram" className="rdsltopt">
+                                Instagram
+                              </option>
+                              <option
+                                value="Referral Link"
+                                className="rdsltopt"
+                              >
+                                Referral link
+                              </option>
+                              <option value="4" className="rdsltopt">
+                                Friend
+                              </option>
+                            </select>
+                            <div className="text-right">
+                              <img src={drpdwnarr} className="drparr" />
+                            </div>
+                          </Col>
+                          <Col md={4} className="slect-inputwrapper">
+                            <div className="tutorsearchbtn">
+                              FIND TUTORS
+                            </div>
+                          </Col>
+                        </Row>
+                      </Col>
+                    </Row>
+                  </div>
+                </div>
+              </Col>
+            </Row>
+            <Row className="dashbdunrdmssgrow">
+              <Col md={5} className="unreadmsswrapp">
+                <div className="unrdmss-title">Unread Messages</div>
+                <img src={yoga} className="img-fluid" />
+                <p>You’re all caught up. No unread messages</p>
+                <div className="centerdbtn">
+                 <span className="unrdmsss-btn">View all Messages</span>
+                </div>
+              </Col>
+              <Col md={7}>
+                <div className="recentlyviewed-section">
+                   <h6 className="recentlyviewed-heading">Tutors Recently viewed</h6>
+                </div>
+              </Col>
+            </Row>
           </Col>
         </Row>
       </Container>
@@ -61,3 +161,28 @@ const parentDashboard = () => {
   );
 };
 export default parentDashboard;
+
+{
+  /* <div className="msstoparentwrapper">
+              <div className="msstoparentcntent">
+                <h4>Hello, David</h4>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit
+                  sunt praesentium veniam qui adipisci voluptates, quam tenetur
+                </p>
+                <div className="viewedprofiles">
+                  <p>Profiles viewed</p>
+                  <span className="fa fa-eye"></span>
+                  <p>10</p>
+                </div>
+                <div className="bookedstatsdiv">
+                  <p className="bookstats">Booked Tutors</p>
+                  <span className="fa fa-file-archive-o"></span>
+                  <p style={{ marginBottom: "0" }}>3</p>
+                </div>
+              </div>
+              <div className="mssgtoparentimage">
+                <img src={project} />
+              </div>
+            </div> */
+}
